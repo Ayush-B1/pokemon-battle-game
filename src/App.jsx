1560cd2cd1deb47de59631b27a086b5746dd87a3
Ayush-B1 <ayush.scb@gmail.com>
2025-04-14 00:57:42 +0530

    Update: Increased background map visibility and reduced overlay opacity

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, Container } from '@chakra-ui/react';
+import { ChakraProvider, Container, Box, Center } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PokemonGame from './components/PokemonGame';
 
@@ -6,11 +6,58 @@ function App() {
   return (
     <ChakraProvider>
       <Router>
-        <Container maxW="container.xl" py={8}>
-          <Routes>
-            <Route path="/" element={<PokemonGame />} />
-          </Routes>
-        </Container>
+        <Box 
+          minH="100vh" 
+          w="100vw" 
+          position="relative"
+          overflow="auto"
+          margin={0}
+          padding={0}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {/* Background Image with transparency */}
+          <Box
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            bgImage="url('/images/kanto-map.png')"
+            bgPosition="center"
+            bgRepeat="no-repeat"
+            bgSize="100% 100%"
+            bgAttachment="fixed"
+            opacity={0.7}
+            zIndex={0}
+          />
+          {/* Dark overlay with less opacity */}
+          <Box
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            bgGradient="linear(to-b, rgba(0,0,0,0.2), rgba(0,0,0,0.1))"
+            zIndex={1}
+            margin={0}
+            padding={0}
+          />
+          <Container 
+            maxW="container.xl" 
+            py={8} 
+            position="relative" 
+            zIndex={2}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Routes>
+              <Route path="/" element={<PokemonGame />} />
+            </Routes>
+          </Container>
+        </Box>
       </Router>
     </ChakraProvider>
   )
